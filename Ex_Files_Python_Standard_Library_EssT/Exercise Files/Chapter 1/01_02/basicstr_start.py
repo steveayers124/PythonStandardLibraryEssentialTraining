4a7d990086a1401ce1ed47d41078ba2dcef76e5e
Steve Ayers <steveayers124@gmail.com>
2019-04-12 15:10:02 -0500

    feat: play with and test the string.join portion of the statement

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_02/basicstr_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_02/basicstr_start.py
@@ -15,6 +15,9 @@ test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of Decemb
 test_string2 = "Supercalifragilistic"
 test_string3 = "90210"
 
+# result = "".join([c for c in test_string1 if c in string.ascii_letters])
+# result = test_string3.join([c for c in test_string1 if c in string.ascii_letters])
+# result = "Z".join([c for c in test_string1 if c in string.ascii_letters])
 result = "".join([c for c in test_string1 if c in string.ascii_letters])
 print(result)
 
