3a389ee46f6ef0628bae4bce3c952df7b575dc3e
Steve Ayers <steveayers124@gmail.com>
2019-04-12 14:56:46 -0500

    feat: use a Python comprehension to filter for only ascii_letters

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_02/basicstr_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_02/basicstr_start.py
@@ -3,10 +3,10 @@ import string
 
 
 # TODO: predefined string constants can be used in common scenarios
-print(string.ascii_letters)
-print(string.digits)
-print(string.hexdigits)
-print(string.punctuation)
+# print(string.ascii_letters)
+# print(string.digits)
+# print(string.hexdigits)
+# print(string.punctuation)
 
 
 
@@ -15,5 +15,6 @@ test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of Decemb
 test_string2 = "Supercalifragilistic"
 test_string3 = "90210"
 
+result = "".join([c for c in test_string1 if c in string.ascii_letters])
 
 # TODO: String testing methods
