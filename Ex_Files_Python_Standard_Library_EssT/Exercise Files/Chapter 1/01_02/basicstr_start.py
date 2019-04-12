7a41702d0a473ab318b22a5171caafadfee78d87
Steve Ayers <steveayers124@gmail.com>
2019-04-12 15:22:35 -0500

    feat: play with and test Python's list comprehension construct

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_02/basicstr_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_02/basicstr_start.py
@@ -15,9 +15,17 @@ test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of Decemb
 test_string2 = "Supercalifragilistic"
 test_string3 = "90210"
 
+# A list comprehension consists of brackets containing an expression 
+# followed by a for clause, then zero or more for or if clauses. 
+# The result will be a new list resulting from evaluating the 
+# expression in the context of the for and if clauses which follow it.
+#  https://docs.python.org/3/tutorial/datastructures.html
 # result = "".join([c for c in test_string1 if c in string.ascii_letters])
 # result = test_string3.join([c for c in test_string1 if c in string.ascii_letters])
 # result = "Z".join([c for c in test_string1 if c in string.ascii_letters])
+# result = "".join(["a" for a in test_string1 if a in string.ascii_letters])
+# result = "".join(["`" for a in test_string1 if a in string.ascii_letters])
+# result = "".join(["`" for a in test_string1 if a in "abc"])
 result = "".join([c for c in test_string1 if c in string.ascii_letters])
 print(result)
 
