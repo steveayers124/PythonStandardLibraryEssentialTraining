516664316d7c8b7b41d8d91b4451b8139967a8b8
Steve Ayers <steveayers124@gmail.com>
2019-04-13 07:29:26 -0500

    feat: learning search tests string.startswith and .endswith

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_03/search_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_03/search_start.py
@@ -4,7 +4,9 @@
 sampleStr = "The quick brown fox jumps over the lazy dog"
 
 # TODO: startsWith and endsWith functions
-
+print(sampleStr.startswith("The"))
+print(sampleStr.startswith("the"))
+print(sampleStr.endswith("dog"))
 
 # TODO: the find and rfind functions
 
