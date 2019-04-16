96b8b918dea376348dc308c36becab5c9983216e
Steve Ayers <steveayers124@gmail.com>
2019-04-16 15:49:01 -0500

    feat: modifiy a string by splitting on a delimiter, and joining with delimiter

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_04/strmanip_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_04/strmanip_start.py
@@ -1,13 +1,17 @@
 # Working with string manipulation functions
 
 test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of December"
+test2 = ["The", "easter", "egg", "is", "for", "Miranda"]
 
 # TODO: upper and lower convert between cases
 print(test_string1.upper())
 print(test_string1.lower())
 
 # TODO: Use the split and join functions
-
+print(test_string1.split(" "))
+print(" ".join(["The", "easter", "egg", "is", "for", "Miranda"]))
+letters = ["a","b","c"]
+print(", ".join(letters))
 
 # TODO: use justification functions to align strings
 # ljust, center, rjust
