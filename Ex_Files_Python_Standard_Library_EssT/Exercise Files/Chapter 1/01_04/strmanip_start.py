6d7598ce1cd34e11de1e5cbe3b4e8909cee3a91f
Steve Ayers <steveayers124@gmail.com>
2019-04-16 15:19:07 -0500

    feat: modifiy a string to upper and lower case by string.upper() and .lower()

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_04/strmanip_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_04/strmanip_start.py
@@ -3,7 +3,8 @@
 test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of December"
 
 # TODO: upper and lower convert between cases
-
+print(test_string1.upper())
+print(test_string1.lower())
 
 # TODO: Use the split and join functions
 
