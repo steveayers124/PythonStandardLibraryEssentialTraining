e6094c34c8f38a11b1d469091f49fd887c2c0570
Steve Ayers <steveayers124@gmail.com>
2019-04-15 23:44:05 -0500

    feat: string.find() from left, first occurence of a substring

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_03/search_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_03/search_start.py
@@ -4,12 +4,12 @@
 sampleStr = "The quick brown fox jumps over the lazy dog"
 
 # TODO: startsWith and endsWith functions
-print(sampleStr.startswith("The"))
-print(sampleStr.startswith("the"))
-print(sampleStr.endswith("dog"))
+# print(sampleStr.startswith("The"))
+# print(sampleStr.startswith("the"))
+# print(sampleStr.endswith("dog"))
 
 # TODO: the find and rfind functions
-
+print(sampleStr.find("the"))
 
 # TODO: using replace
 
