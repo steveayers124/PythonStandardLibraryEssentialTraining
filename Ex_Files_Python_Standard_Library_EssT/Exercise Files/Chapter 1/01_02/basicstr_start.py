f4f76def5c517295ba3f944b7368e4177723f2a4
Steve Ayers <steveayers124@gmail.com>
2019-04-17 23:31:40 -0500

    fix: uncomment code that I had tested

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_02/basicstr_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_02/basicstr_start.py
@@ -21,77 +21,77 @@ print(string.octdigits)
 print("\n\n\n")
 
 
-# # TODO: Use the constants to filter information out
-# test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of December"
-# test_string2 = "Supercalifragilistic"
-# test_string3 = "90210"
+# TODO: Use the constants to filter information out
+test_string1 = "The quick brown fox jumps over the lazy dog on the 1st of December"
+test_string2 = "Supercalifragilistic"
+test_string3 = "90210"
 
-# # A list comprehension consists of brackets containing an expression 
-# # followed by a for clause, then zero or more for or if clauses. 
-# # The result will be a new list resulting from evaluating the 
-# # expression in the context of the for and if clauses which follow it.
-# #  https://docs.python.org/3/tutorial/datastructures.html
-# result = "".join([c for c in test_string1 if c in string.ascii_letters])
-# print(result)
-# result = test_string3.join([c for c in test_string1 if c in string.ascii_letters])
-# print(result)
-# result = "Z".join([c for c in test_string1 if c in string.ascii_letters])
-# print(result)
-# result = "".join(["a" for a in test_string1 if a in string.ascii_letters])
-# print(result)
-# result = "".join(["`" for a in test_string1 if a in string.ascii_letters])
-# print(result)
-# result = "".join(["`" for a in test_string1 if a in "abc"])
-# print(result)
-# result = "".join([c for c in test_string1 if c in string.ascii_letters])
-# print(result)
+# A list comprehension consists of brackets containing an expression 
+# followed by a for clause, then zero or more for or if clauses. 
+# The result will be a new list resulting from evaluating the 
+# expression in the context of the for and if clauses which follow it.
+#  https://docs.python.org/3/tutorial/datastructures.html
+result = "".join([c for c in test_string1 if c in string.ascii_letters])
+print(result)
+result = test_string3.join([c for c in test_string1 if c in string.ascii_letters])
+print(result)
+result = "Z".join([c for c in test_string1 if c in string.ascii_letters])
+print(result)
+result = "".join(["a" for a in test_string1 if a in string.ascii_letters])
+print(result)
+result = "".join(["`" for a in test_string1 if a in string.ascii_letters])
+print(result)
+result = "".join(["`" for a in test_string1 if a in "abc"])
+print(result)
+result = "".join([c for c in test_string1 if c in string.ascii_letters])
+print(result)
 
-# print("\n\n\n")
-
-
-# # TODO: String testing methods
-
-# print(test_string1.isalnum())
-# print(test_string2.isalpha())
-
-# print(all([c.isalpha() for c in test_string1]))
-# print(all([c.isalpha() for c in test_string2]))
-# print(all([c.isalpha() for c in test_string3]))
-
-# print(all([c.isalnum() for c in test_string1]))
-# print(all([c.isalnum() for c in test_string2]))
-# print(all([c.isalnum() for c in test_string3]))
-
-# print(all([c.isnumeric() for c in test_string1]))
-# print(all([c.isnumeric() for c in test_string2]))
-# print(all([c.isnumeric() for c in test_string3]))
-
-# print("\n\n\n")
+print("\n\n\n")
 
 
-# # TODO: more playing around
-# print("".join([c for c in test_string1 if c in string.ascii_letters]))
-# print("".join([c for c in test_string2 if c in string.ascii_letters]))
-# print("".join([c for c in test_string3 if c in string.ascii_letters]))
+# TODO: String testing methods
 
-# print("".join([c for c in test_string1 if c in string.digits]))
-# print("".join([c for c in test_string2 if c in string.digits]))
-# print("".join([c for c in test_string3 if c in string.digits]))
+print(test_string1.isalnum())
+print(test_string2.isalpha())
 
-# print("".join([c for c in test_string1 if c in string.hexdigits]))
-# print("".join([c for c in test_string2 if c in string.hexdigits]))
-# print("".join([c for c in test_string3 if c in string.hexdigits]))
+print(all([c.isalpha() for c in test_string1]))
+print(all([c.isalpha() for c in test_string2]))
+print(all([c.isalpha() for c in test_string3]))
 
-# print("\n\n\n")
+print(all([c.isalnum() for c in test_string1]))
+print(all([c.isalnum() for c in test_string2]))
+print(all([c.isalnum() for c in test_string3]))
 
-# print(any([c.isalpha() for c in test_string1]))
-# print(any([c.isalpha() for c in test_string2]))
-# print(any([c.isalpha() for c in test_string3]))
+print(all([c.isnumeric() for c in test_string1]))
+print(all([c.isnumeric() for c in test_string2]))
+print(all([c.isnumeric() for c in test_string3]))
 
-# print(any([c.isalnum() for c in test_string1]))
-# print(any([c.isalnum() for c in test_string2]))
-# print(any([c.isalnum() for c in test_string3]))
+print("\n\n\n")
 
-# print(any([c.isnumeric() for c in test_string1]))
-# print(any([c.isnumeric() for c in test_string2]))
-# print(any([c.isnumeric() for c in test_string3]))
+
+# TODO: more playing around
+print("".join([c for c in test_string1 if c in string.ascii_letters]))
+print("".join([c for c in test_string2 if c in string.ascii_letters]))
+print("".join([c for c in test_string3 if c in string.ascii_letters]))
+
+print("".join([c for c in test_string1 if c in string.digits]))
+print("".join([c for c in test_string2 if c in string.digits]))
+print("".join([c for c in test_string3 if c in string.digits]))
+
+print("".join([c for c in test_string1 if c in string.hexdigits]))
+print("".join([c for c in test_string2 if c in string.hexdigits]))
+print("".join([c for c in test_string3 if c in string.hexdigits]))
+
+print("\n\n\n")
+
+print(any([c.isalpha() for c in test_string1]))
+print(any([c.isalpha() for c in test_string2]))
+print(any([c.isalpha() for c in test_string3]))
+
+print(any([c.isalnum() for c in test_string1]))
+print(any([c.isalnum() for c in test_string2]))
+print(any([c.isalnum() for c in test_string3]))
+
+print(any([c.isnumeric() for c in test_string1]))
+print(any([c.isnumeric() for c in test_string2]))
+print(any([c.isnumeric() for c in test_string3]))
