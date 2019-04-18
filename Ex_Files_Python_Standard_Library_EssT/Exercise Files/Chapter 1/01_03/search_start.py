5be5fc4ff7b649802b5be3d4e26cbba16bbcf97b
Steve Ayers <steveayers124@gmail.com>
2019-04-17 23:37:20 -0500

    fix: uncomment code that I had tested

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_03/search_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_03/search_start.py
@@ -4,30 +4,30 @@
 sampleStr = "The quick brown fox jumps over the lazy dog"
 
 # TODO: startsWith and endsWith functions
-# print(sampleStr.startswith("The"))
-# print(sampleStr.startswith("the"))
-# print(sampleStr.endswith("dog"))
+print(sampleStr.startswith("The"))
+print(sampleStr.startswith("the"))
+print(sampleStr.endswith("dog"))
 
 # TODO: the find and rfind functions
-# print(sampleStr.find("the"))
-# print(sampleStr.rfind("the"))
-# print("the" in sampleStr)
+print(sampleStr.find("the"))
+print(sampleStr.rfind("the"))
+print("the" in sampleStr)
 
 # TODO: using replace
 newStr = sampleStr.replace("lazy", "tired")
 print(newStr)
-# newStr = sampleStr.replace("lazy", "")
-# print(newStr)
-# newStr = sampleStr.replace("o", "~")
-# print(newStr)
-# newStr = sampleStr.replace("o", "~", 1)
-# print(newStr)
-# newStr = sampleStr.replace("o", "~", -1)
-# print(newStr)
-# newStr = sampleStr.replace("o", "~", 3)
-# print(newStr)
-# newStr = sampleStr.replace("o", "~", 0)
-# print(newStr)
+newStr = sampleStr.replace("lazy", "")
+print(newStr)
+newStr = sampleStr.replace("o", "~")
+print(newStr)
+newStr = sampleStr.replace("o", "~", 1)
+print(newStr)
+newStr = sampleStr.replace("o", "~", -1)
+print(newStr)
+newStr = sampleStr.replace("o", "~", 3)
+print(newStr)
+newStr = sampleStr.replace("o", "~", 0)
+print(newStr)
 
 # TODO: counting instances of substrings
 print(sampleStr.count("over"))
