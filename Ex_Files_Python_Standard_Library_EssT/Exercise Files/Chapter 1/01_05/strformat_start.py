0db9b8cb0c4bbdc2aa040bf8565f2af71da46709
Steve Ayers <steveayers124@gmail.com>
2019-04-24 22:22:27 -0500

    feat: find other formatting directives - Octal, Hex(lower case), Hex(upper case), Binary, Decimal, and map to ASCII character

--- Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_05/strformat_start.py
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_05/strformat_start.py
@@ -27,8 +27,8 @@ print("Today's date is {0}. On {0}, you should go to {1}.\nYou should go to {1} 
 # using named variables
 print("Output: {var1}, {var2}".format(var1=foo,var2=bar))
 # But you can also use formatting directives- lower and upper case hex
-print("Output: Octal {var2:o}, lowerHex {var2:x}, upperHex {var2:X}, {var1}".format(var1=foo,var2=bar))
-#print("Output: Octal-|{var2:o}|, lowerHex-|{var2:x}|, upperHex|{var2:X}|, {var1}".format(var1=foo,var2=bar))
+print("Output: Octal |{var2:o}|, lowerHex |{var2:x}|, upperHex |{var2:X}|, {var1}".format(var1=foo,var2=bar))
+print("Output: Binary |{var2:b}|, Decimal |{var2:d}|, Chacter |{var2:c}|, {var1}".format(var1=foo,var2=bar))
 
 
 
