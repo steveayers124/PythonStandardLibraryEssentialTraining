e111f3847719ff3c433bcabc0ae9109b0c54c02a
Steve Ayers <steveayers124@gmail.com>
2019-05-01 16:27:04 -0500

    feat: try out math.ceil()

---
+++ Ex_Files_Python_Standard_Library_EssT/Exercise Files/Chapter 1/01_05/mathMyExample.py
@@ -0,0 +1,14 @@
+import math
+
+print("ceiling of")
+f1 = 4.0
+print(f"  math.ceil({f1}) = |{math.ceil(f1)}|")
+f1 = 4.1
+print(f"  math.ceil({f1}) = |{math.ceil(f1)}|")
+f1 = 4.5
+print(f"  math.ceil({f1}) = |{math.ceil(f1)}|")
+f1 = 4.9
+print(f"  math.ceil({f1}) = |{math.ceil(f1)}|")
+i1 = 3
+print(f"  math.ceil({i1}) = |{math.ceil(i1)}|")
+
